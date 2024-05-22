c2fb80ae11910b51635c765ef90c682bd49b44fa
nthnptt <yaman0@protonmail.com>
2024-05-22 21:57:41 +0200

    feat(nvim): Gen

--- nvim/.config/nvim/lua/plugins/gen.lua
+++ nvim/.config/nvim/lua/plugins/gen.lua
@@ -1,3 +1,9 @@
 return {
   "David-Kunz/gen.nvim",
+  config = function()
+    require("gen").prompts["Builder_function_parameters"] = {
+      prompt = "Add parameters of object created in this function as parameters of this function. \nExample :\n<BEFORE>\npublic function cow(): Cow {\n  return new Cow(\n    name: 'cow',\n    weight: 128,\n    color: CowColor:WHITE,\n    farm: new Farm('my farm'),\n  )\n}\n</BEFORE>\n<EXPECTED>\npublic function cow(\n    string $name = 'cow',\n    int $weight = 128,\n    Color $color = CowColor:WHITE,\n    ?Farm $farm = null,\n): Cow {\n  return new Cow(\n    name: $name,\n    weight: $weight,\n    color: $color,\n    farm: farm ?? new Farm('my farm'),\n  )\n}\n</EXPECTED>\n Code: $text Expected:",
+      replace = true,
+    }
+  end,
 }
